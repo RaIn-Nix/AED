d8ba62414232c838c53ce42d9a6cb28686647ed6
Ayelén Mariscal <114955549+RaIn-Nix@users.noreply.github.com>
2023-04-19 20:03:50 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/Hello.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Ayelen Coca Mariscal
+// Version     : 19/04/2023
+// Copyright   : Your copyright notice
+// Description : Hello World
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hallo Welt!!!"<<endl; //prints !!!Hallo Welt!!!
+	printf ("!!!Hello World!!!");
+	return 0;
+}
